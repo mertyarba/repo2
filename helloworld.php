516c95aaa1ec18112b2e2bad5533630d9965d8e8
mertyarba <mertyarba@greeny.cs.tlu.ee>
2016-02-11 12:53:34 +0200

    added age

--- helloworld.php
+++ helloworld.php
@@ -13,4 +13,25 @@
 	//for adding up numbers "+" sign
 	//for adding up strings "." sign 
 	echo $first_name.$last_name;
+	
+	// > greater than
+	// < smaller than
+	// >= greater than or equal
+	// <= smaller than or equal
+	// == equal
+	// != not equal
+	
+	$age = 5;
+	
+	// if age is smaller than or equal to 6, then you can sleep at lunch time
+	if ($age <= 6){
+		
+	
+		//its true
+		echo " yes you can sleep at lunch time"	;
+		
+	}else{
+		//its false
+		echo "no, you are at school";
+	}
 ?>
