71f041ee9321e989aa369250cb1241b7e69f0cfe
mertyarba <mertyarba@greeny.cs.tlu.ee>
2016-02-11 12:36:08 +0200

    added bunch of codes

--- helloworld.php
+++ helloworld.php
@@ -1,5 +1,15 @@
 <?php
+	//variable starts with $
+	//string has to use quotation marks
+	//line ends with semicolon
 
-//hello
 
+
+	$first_name = "Mert";
+
+
+	//show code to browser
+	//for adding up numbers "+" sign
+	//for adding up strings "." sign 
+	echo $first_name.$last_name;
 ?>
